0a270791e9fda52598c87e4491625841fe89193e
Fernando Gonzalez <fernando.gonzalez@ideit.es>
2018-11-15 16:58:54 +0100

    Remove author and add type in entry meta.

--- views/partials/entry-meta.blade.php
+++ views/partials/entry-meta.blade.php
@@ -1,6 +1,9 @@
 <div class="entry-meta">
+  {{--
   <p class="byline author vcard">
     <a href="{{ get_author_posts_url(get_the_author_meta('ID')) }}" rel="author" class="fn">{{ get_the_author() }}</a>
   </p>
+  --}}
   <time class="updated" datetime="{{ get_post_time('c', true) }}">{{ get_the_date() }}</time>
+  <span class="entry-type">| {{ UVigoThemeWPApp::getPostTypeTitle(get_post_type()) }}</span>
 </div>
