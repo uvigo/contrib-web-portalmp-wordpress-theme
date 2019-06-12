3bceda9999a72a775a8a148e5997e42a9c700e3c
Fernando Gonzalez <fernando.gonzalez@ideit.es>
2019-06-12 13:02:47 +0200

    Cambios en el build.

--- resources/assets/build/webpack.config.optimize.js
+++ resources/assets/build/webpack.config.optimize.js
@@ -12,7 +12,7 @@ module.exports = {
       optipng: { optimizationLevel: 7 },
       gifsicle: { optimizationLevel: 3 },
       pngquant: { quality: '65-90', speed: 4 },
-      svgo: { removeUnknownsAndDefaults: false, cleanupIDs: false },
+      svgo: { removeUnknownsAndDefaults: false, cleanupIDs: false, removeViewBox: false },
       plugins: [imageminMozjpeg({ quality: 75 })],
       disable: (config.enabled.watcher),
     }),
