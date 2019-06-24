9df0b8e03ee1f187d0e9907c98d97a5c5cdde7e9
Paula <ritapaula.fernandez@ideit.es>
2019-06-24 11:05:58 +0200

    Modifica plantilla de ofertas de emprego y prácticas extracurriculares

--- views/partials/content-single-uvigo-offer.blade.php
+++ views/partials/content-single-uvigo-offer.blade.php
@@ -18,11 +18,15 @@
     </div>
   </header>
   <div class="entry-content page-content">
+    <p class="description-title"><strong>Referencia:</strong> {{ get_post_meta(get_the_ID(), 'uvigo_offers_offer_reference', true) }}</p>
+    <p class="description-title"><strong>Empresa:</strong> {{ get_post_meta(get_the_ID(), 'uvigo_offers_offer_company', true) }}</p>
+    <p class="description-title"><strong>Lugar de traballo:</strong> {{ get_post_meta(get_the_ID(), 'uvigo_offers_offer_workplace', true) }}</p>
     @php($start_date =  mysql2date( 'U', get_post_meta(get_the_ID(), 'uvigo_offers_offer_start_date', true), false ))
     @php($end_date =  mysql2date( 'U', get_post_meta(get_the_ID(), 'uvigo_offers_offer_end_date', true), false ))
-    <p class="description-title"><strong>Inicio publicación:</strong> {{ date(get_option('date_format'), $start_date) }}</p>
-    <p class="description-title"><strong>Fin publicación:</strong> {{ date(get_option('date_format'), $end_date) }}</p>
+    <!-- <p class="description-title"><strong>Inicio publicación:</strong> {{ date(get_option('date_format'), $start_date) }}</p> -->
+    <p class="description-title"><strong>Data fin:</strong> {{ date(get_option('date_format'), $end_date) }}</p>
     @php(the_content())
+    <p class="description-url">Máis información na área de emprego da UVigo: <a href="{{ get_post_meta(get_the_ID(), 'uvigo_offers_offer_url', true) }}" target="_blank">{{ get_post_meta(get_the_ID(), 'uvigo_offers_offer_url', true) }}</a></p>
   </div>
   <footer>
     {!! wp_link_pages(['echo' => 0, 'before' => '<nav class="page-nav"><p>' . __('Pages:', 'uvigothemewp'), 'after' => '</p></nav>']) !!}
--- views/partials/content-single-uvigo-practice.blade.php
+++ views/partials/content-single-uvigo-practice.blade.php
@@ -18,11 +18,15 @@
     </div>
   </header>
   <div class="entry-content page-content">
+    <p class="description-title"><strong>Referencia:</strong> {{ get_post_meta(get_the_ID(), 'uvigo_offers_practice_reference', true) }}</p>
+    <p class="description-title"><strong>Empresa:</strong> {{ get_post_meta(get_the_ID(), 'uvigo_offers_practice_company', true) }}</p>
+    <p class="description-title"><strong>Lugar de traballo:</strong> {{ get_post_meta(get_the_ID(), 'uvigo_offers_practice_workplace', true) }}</p>
     @php($start_date =  mysql2date( 'U', get_post_meta(get_the_ID(), 'uvigo_offers_practice_start_date', true), false ))
     @php($end_date =  mysql2date( 'U', get_post_meta(get_the_ID(), 'uvigo_offers_practice_end_date', true), false ))
-    <p class="description-title"><strong>Inicio publicación:</strong> {{ date(get_option('date_format'), $start_date) }}</p>
-    <p class="description-title"><strong>Fin publicación:</strong> {{ date(get_option('date_format'), $end_date) }}</p>
+    <!-- <p class="description-title"><strong>Inicio publicación:</strong> {{ date(get_option('date_format'), $start_date) }}</p> -->
+    <p class="description-title"><strong>Data fin:</strong> {{ date(get_option('date_format'), $end_date) }}</p>
     @php(the_content())
+    <p class="description-url">Máis información na área de práticas extracurriculares da UVigo: <a href="{{ get_post_meta(get_the_ID(), 'uvigo_offers_practice_url', true) }}" target="_blank">{{ get_post_meta(get_the_ID(), 'uvigo_offers_practice_url', true) }}</a></p>
   </div>
   <footer>
     {!! wp_link_pages(['echo' => 0, 'before' => '<nav class="page-nav"><p>' . __('Pages:', 'uvigothemewp'), 'after' => '</p></nav>']) !!}
